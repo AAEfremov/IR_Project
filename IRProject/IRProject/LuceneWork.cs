cfbe3ccb1e0f2aeff6e950c268c8a6837766d54f
AAEfremov <aleks180694@mail.ru>
2016-03-08 15:04:58 +0300

    Adding project files

---
+++ IRProject/IRProject/LuceneWork.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+
+namespace IRProject
+{
+    class LuceneWork
+    {
+         private Analyzer analyzer = new WhitespaceAnalyzer(); 
+        private Directory luceneIndexDirectory;
+        private IndexWriter writer;
+        private string indexPath = @"C:\lucene";
+
+        public LuceneWork()
+        {
+            InitialiseLucene();
+        }
+
+        private void InitialiseLucene()
+        {
+            if(System.IO.Directory.Exists(indexPath))
+            {
+                System.IO.Directory.Delete(indexPath,true);
+            }
+
+            luceneIndexDirectory =FSDirectory.Open(indexPath);
+            writer = new IndexWriter(luceneIndexDirectory,analyzer,IndexWriter.MaxFieldLength.LIMITED);
+        }
+
+        public void BuildIndex(List<DocStructure> dataToIndex)
+        {
+            foreach (var sampleDataFileRow in dataToIndex)
+	        {
+		        Document doc = new Document();
+                Console.Write("kek");
+                doc.Add(new Field("mark", sampleDataFileRow.name, Field.Store.YES, Field.Index.ANALYZED));
+                doc.Add(new Field("review", sampleDataFileRow.review, Field.Store.YES, Field.Index.ANALYZED));
+                writer.AddDocument(doc);
+	        }
+            writer.Optimize();
+            //writer.Flush(true,true,true);
+            writer.Dispose();
+            //luceneIndexDirectory.Dispose();
+        }
+
+        public List<DocStructure> Search(string searchTerm)
+        {
+            IndexSearcher searcher = new IndexSearcher(luceneIndexDirectory);
+            QueryParser parser = new QueryParser(Lucene.Net.Util.Version.LUCENE_30,"LineText", analyzer);
+
+            Query query = parser.Parse(searchTerm);
+            TopScoreDocCollector cllctr = TopScoreDocCollector.Create(100, true);
+            searcher.Search(query, cllctr); 
+            ScoreDoc[] hits = cllctr.TopDocs().ScoreDocs;
+
+            List<DocStructure> results = new List<DocStructure>();
+            DocStructure sampleDataFileRow = null;
+          
+            for (int i = 0; i < hits.Length; i++)
+            {
+                sampleDataFileRow = new DocStructure();
+                int docId = hits[i].Doc;
+                float score = hits[i].Score;
+                Document doc = searcher.Doc(docId);
+                sampleDataFileRow.name =doc.Get("mark");
+                sampleDataFileRow.review = doc.Get("review");
+                //sampleDataFileRow.Score = score;
+
+                results.Add(sampleDataFileRow);
+            }
+           
+            return results;
+        }
+    }
+}
---
+++ IRProject/IRProject/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+
+namespace IRProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<DocStructure> mylist = new List<DocStructure>();
+            string place = @"C:\Users\Элона\Documents\Visual Studio 2012\Projects\ConsoleApplication5\ConsoleApplication5\bin\Debug\text";
+            parser(mylist, place);
+            var l = new LuceneWork();
+            l.BuildIndex(mylist);
+            
+            Console.WriteLine("write");
+            var a = Console.ReadLine();
+            var res = l.Search(a);
+            Console.WriteLine(res.Count());
+
+
+
+            Console.ReadLine();
+
+        }
+
+        static void parser(List<DocStructure> mylist, string place)
+        {
+            string help;
+            string name;
+            
+            DirectoryInfo dir = new DirectoryInfo(place);
+            foreach (var item in dir.GetFiles())
+            {
+                name = item.FullName;
+
+                string text = System.IO.File.ReadAllText(name);
+
+                //reviews
+                string pattern = @"(?<=<TEXT>)(.*)(?=</TEXT>)";
+                Regex rgx = new Regex(pattern);
+
+                foreach (Match match in rgx.Matches(text))
+                {
+                    DocStructure doc1 = new DocStructure();
+                    doc1.review = match.Value;
+                    mylist.Add(doc1);
+                }
+                //fav
+                pattern = @"(?<=<FAVORITE>)(.*)(?=</FAVORITE>)";
+                rgx = new Regex(pattern);
+                int i = 0;
+                foreach (Match match in rgx.Matches(text))
+                {
+                    mylist[i].fav = match.Value;
+                    i++;
+
+                }
+                help = Regex.Match(text, @"(?<=<DOCNO>)(.*)(?=</DOCNO>)").ToString();
+                help = help.Remove(0, 5);
+                help = help.Replace("_", " ");
+                //Console.WriteLine(help);
+                for (i = 0; i < mylist.Count; i++)
+                {
+                   
+                    mylist[i].name = help;
+
+                }
+
+            }
+        }
+    }
+}
